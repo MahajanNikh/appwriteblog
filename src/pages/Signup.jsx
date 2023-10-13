61bdeda77e94f6d58f4bc43cc6daa4ebf5f98b71
Nikhil Mahajan <mahajan12647@gmail.com>
2023-10-13 14:49:44 +0530

    first commit

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Signup as Scomponent } from '../components'
+
+function Signup() {
+  debugger
+  return (
+    <div className='py-8'>
+      <Scomponent />
+    </div>
+  )
+}
+
+export default Signup
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import AddPost from "./AddPost";
+import AllPosts from "./AllPosts";
+import EditPost from "./EditPost.jsx";
+import Home from "./Home";
+import Login from "./Login";
+import Post from "./post";
+import Signup from './Signup'
+
+export { AddPost, AllPosts, EditPost, Home, Login, Post, Signup };
